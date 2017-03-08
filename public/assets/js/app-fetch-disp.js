a28baf1429dfc00e1b322cc02caa8a98dc0fec4c
Joey Scott <joannascott29@hotmail.co.uk>
2017-03-08 15:56:40 +0000

    Add generic API call function Relates #17

---
+++ public/assets/js/app-fetch-disp.js
@@ -0,0 +1,11 @@
+function request(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonObj = JSON.parse(xhr.responseText);
+      cb(jsonObj);
+    }
+  }
+  xhr.open('GET', url, true);
+  xhr.send();
+}
