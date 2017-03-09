83fa39fbc5e108199a7cec5dce249b034e963edc
Zooey <zooey@lbk.org.uk>
2017-03-09 17:08:16 +0000

    add id to body to aid testing

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -133,7 +133,7 @@ function displayData(err, appData) {
 
   for (var key in appData.display) {
     if (key === 'image') {
-      document.body.style.backgroundImage = 'url("' + appData.display[key] + '")';
+      document.getElementById("js-body").style.backgroundImage = 'url("' + appData.display[key] + '")';
     } else {
       document.querySelector(`.${key}`).textContent = appData.display[key];
     }
