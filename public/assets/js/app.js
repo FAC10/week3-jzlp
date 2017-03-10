ca8ef35bf769dc99a1e97febde2d84ad9cf5c587
PiotrBerebecki <petebere@gmail.com>
2017-03-10 00:49:25 +0000

    Abstract out api dialogue into generic function

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,19 +1,41 @@
 // ***************************************************************
 // APP
 // ***************************************************************
-waterfall({display: {}}, [getLocation, getWeather, getImage], displayData);
-
+waterfall({
+  toBeDisplayed: {},
+  info: {}
+}, [
+  [getResource, makeLocationUrl, mergeLocation],
+  [getResource, makeWeatherUrl, mergeWeather],
+  [getResource, makeImageUrl, mergeImage]
+],
+displayData);
 
 
 function waterfall(appData, tasks, finalCallback) {
   if (tasks.length === 0) {
     return finalCallback(null, appData);
   }
-  tasks[0](appData, function(err, res) {
+  tasks[0][0](appData, ...tasks[0].slice(1), function(err, updatedAppData) {
     if (err) {
       return finalCallback(err);
     }
-    waterfall(res, tasks.slice(1), finalCallback);
+    waterfall(updatedAppData, tasks.slice(1), finalCallback);
+  });
+}
+
+
+
+
+// ***************************************************************
+// API RESOURCE GETTER
+// ***************************************************************
+function getResource(appData, makeUrl, mergeResource, updatedDataCallback) {
+  fetch('GET', makeUrl(appData), function(err, jsonResponseObject) {
+    if (err) {
+      return updatedDataCallback(err);
+    }
+    updatedDataCallback(null, mergeResource(appData, jsonResponseObject));
   });
 }
 
@@ -23,25 +45,15 @@ function waterfall(appData, tasks, finalCallback) {
 // ***************************************************************
 // LOCATION
 // ***************************************************************
-function getLocation(appData, handleUpdatedDataCallback) {
-  fetch('GET', makeLocationUrl(appData), function(err, jsonResponseObject) {
-    if (err) {
-      return handleUpdatedDataCallback(err);
-    }
-    handleUpdatedDataCallback(null, mergeLocation(appData, jsonResponseObject));
-  });
-}
-
-
-function makeLocationUrl(appData){
-  return 'https://geoip.nekudo.com/api/';
-}
-
-
 function mergeLocation(appData, jsonResponseObject) {
-  appData.latitude = jsonResponseObject.location.latitude;
-  appData.longitude = jsonResponseObject.location.longitude;
+  appData.info.latitude = jsonResponseObject.location.latitude;
+  appData.info.longitude = jsonResponseObject.location.longitude;
   return appData;
+}
+
+
+function makeLocationUrl(appData) {
+  return 'https://geoip.nekudo.com/api/';
 }
 
 
@@ -50,54 +62,37 @@ function mergeLocation(appData, jsonResponseObject) {
 // ***************************************************************
 // WEATHER
 // ***************************************************************
-function getWeather(appData, handleUpdatedDataCallback) {
-  fetch('GET', makeWeatherUrl(appData), function(err, jsonResponseObject) {
-    if (err) {
-      return handleUpdatedDataCallback(err);
-    }
-    handleUpdatedDataCallback(null, mergeWeather(appData, jsonResponseObject));
-  });
+function mergeWeather(appData, jsonResponseObject) {
+  appData.toBeDisplayed.city = jsonResponseObject.name;
+  appData.toBeDisplayed.temperature = `${Math.round(parseFloat(jsonResponseObject.main.temp))}°C`;
+  appData.toBeDisplayed.summary = jsonResponseObject.weather[0].main;
+  appData.info.description = jsonResponseObject.weather[0].description;
+  return appData;
 }
 
 
 function makeWeatherUrl(appData) {
-  return `http://api.openweathermap.org/data/2.5/weather?lat=${appData.latitude}&lon=${appData.longitude}&appid=${openWeatherKey}&units=metric`; // eslint-disable-line no-undef
-}
-
-
-function mergeWeather(appData, jsonResponseObject) {
-  appData.display.city = jsonResponseObject.name;
-  appData.display.temperature = `${jsonResponseObject.main.temp}°C`;
-  appData.display.summary = jsonResponseObject.weather[0].main;
-  appData.description = jsonResponseObject.weather[0].description;
-  return appData;
+  return `http://api.openweathermap.org/data/2.5/weather?lat=${appData.info.latitude}&lon=${appData.info.longitude}&appid=${openWeatherKey}&units=metric`; // eslint-disable-line no-undef
 }
 
 
 
 
 // ***************************************************************
-// IMAGES
+// IMAGE
 // ***************************************************************
-function getImage(appData, handleUpdatedDataCallback) {
-  fetch('GET', makeImageUrl(appData), function(err, jsonResponseObject) {
-    if (err) {
-      return handleUpdatedDataCallback(err);
-    }
-    handleUpdatedDataCallback(null, mergeImage(appData, jsonResponseObject));
+function mergeImage(appData, jsonResponseObject) {
+  appData.toBeDisplayed.images = jsonResponseObject.data.map(function(item) {
+    return item.images.downsized_medium.url;
   });
+
+  return appData;
 }
 
 
 function makeImageUrl(appData) {
-  var encodedDescription = encodeURIComponent(appData.description);
+  var encodedDescription = encodeURIComponent(appData.toBeDisplayed.summary);
   return `http://api.giphy.com/v1/gifs/search?q=${encodedDescription}&api_key=dc6zaTOxFJmzC`;
-}
-
-
-function mergeImage(appData, jsonResponseObject) {
-  appData.display.image = jsonResponseObject.data[0].images.downsized_medium.url;
-  return appData;
 }
 
 
@@ -106,18 +101,17 @@ function mergeImage(appData, jsonResponseObject) {
 // ***************************************************************
 // FETCH
 // ***************************************************************
-function fetch(method, url, handleResponseCallback) {
-  var xhr = new XMLHttpRequest();
+function fetch(method, url, responseCallback) {
+  const xhr = new XMLHttpRequest();
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      var jsonObj = JSON.parse(xhr.responseText);
-      handleResponseCallback(null, jsonObj);
+      responseCallback(null, JSON.parse(xhr.responseText));
     }
   };
 
   xhr.onerror = function() {
-    handleResponseCallback('Api response error');
+    responseCallback('Api response error');
   };
 
   xhr.open(method, url, true);
@@ -135,14 +129,55 @@ function displayData(err, appData) {
     document.querySelector(`.summary`).textContent = 'Sorry, data unavailable';
     return Error;
   }
-
-  for (var key in appData.display) {
-    if (key === 'image') {
-
-      document.getElementById("js-body").style.backgroundImage = `url("${appData.display[key]}")`;
+  for (let key in appData.toBeDisplayed) {
+    if (key === 'images') {
+      animateImage(appData.toBeDisplayed.images, 5000);
 
     } else {
-      document.querySelector(`.${key}`).textContent = appData.display[key];
+      document.querySelector(`.${key}`).textContent = appData.toBeDisplayed[key];
     }
   }
+}
+
+
+function animateImage(images, delay) {
+  let imageCounter = 0;
+  let imageDummyElement;
+  const imagesDOM = document.querySelector('.images');
+  imagesDOM.src = images[imageCounter];
+
+  setTimeout(function() {
+    imagesDOM.style.opacity = 0;
+  }, delay - 1000);
+  setTimeout(function() {
+    imagesDOM.style.opacity = 1;
+  }, delay + 200);
+
+  setInterval(function() {
+    setTimeout(function() {
+      imagesDOM.style.opacity = 0;
+    }, delay - 1000);
+    setTimeout(function() {
+      imagesDOM.style.opacity = 1;
+    }, delay + 200);
+
+
+    imageCounter = (++imageCounter) % images.length;
+    // Preload the following image for smoother experience
+    imageDummyElement = new Image();
+    imageDummyElement.src = images[(imageCounter+1) % images.length];
+    console.log(imageCounter);
+    imagesDOM.src = images[imageCounter];
+  }, delay);
+}
+
+
+
+
+
+// ***************************************************************
+// HELPERS
+// ***************************************************************
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 }
